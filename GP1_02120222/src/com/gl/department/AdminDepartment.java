ff297313db81cd7e58d44f40533d746527c38f2e
manuroy <manuroy@CSCO-W-PF462VY8.cisco.com>
2022-12-02 14:29:17 +0530

    complete changes for admin

--- GP1_02120222/src/com/gl/department/AdminDepartment.java
+++ GP1_02120222/src/com/gl/department/AdminDepartment.java
@@ -1,5 +1,21 @@
 package com.gl.department;
 
-public class AdminDepartment {
+public class AdminDepartment extends SuperDepartment {
+	
+	public AdminDepartment(){
+		System.out.println("Welcome to Admin Department");
+	}
 
+	public String departmentName() {
+		valDepartmentName="Admin Department";
+		return valDepartmentName;
+	}
+	public String getTodaysWork() {
+		valGetTodaysWork = "Complete your documents Submission";
+		return valGetTodaysWork;
+	}
+	public String getWorkDeadline() {
+		valGetWorkDeadline="Complete by EOD";
+		return valGetWorkDeadline;
+	}
 }
