befc3e5dd1fe1bcd3628df362de10e68bddfc1b8
manuroy <manuroy@CSCO-W-PF462VY8.cisco.com>
2022-12-02 14:28:54 +0530

    changes complete

--- GP1_02120222/src/DriverMain.java
+++ GP1_02120222/src/DriverMain.java
@@ -4,7 +4,12 @@ import com.gl.department.*;
 public class DriverMain {
 
 	public static void main(String[] args) {
-		
+		// Admin Department
+		SuperDepartment obj1 = new AdminDepartment();
+		System.out.println(obj1.departmentName());
+		System.out.println(obj1.getTodaysWork());
+		System.out.println(obj1.getWorkDeadline());
+		System.out.println(obj1.isTodayAHoliday());
 
 	}
 
