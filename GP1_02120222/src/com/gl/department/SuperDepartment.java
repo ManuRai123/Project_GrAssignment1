20a44134b5fd9c7a7b0e11602b20e2b5b56e91bd
manuroy <manuroy@CSCO-W-PF462VY8.cisco.com>
2022-12-02 14:05:02 +0530

    added function to Super class

--- GP1_02120222/src/com/gl/department/SuperDepartment.java
+++ GP1_02120222/src/com/gl/department/SuperDepartment.java
@@ -1,13 +1,26 @@
 package com.gl.department;
 
 public class SuperDepartment {
-	
+	String valDepartmentName;
+	String valGetTodaysWork;
+	String valGetWorkDeadline;
+	String valIsTodayAHoliday;
+
 	public String departmentName() {
-		
+		valDepartmentName="Super Department";
+		return valDepartmentName;
 	}
-	public String getTodaysWork() {}
-	public String getWorkDeadline() {}
-	public String isTodayAHoliday() {}
-	
+	public String getTodaysWork() {
+		valGetTodaysWork = "No Work as of now";
+		return valGetTodaysWork;
+	}
+	public String getWorkDeadline() {
+		valGetWorkDeadline="Nil";
+		return valGetWorkDeadline;
+	}
+	public String isTodayAHoliday() {
+		valIsTodayAHoliday= "Today is not a holiday";
+		return valIsTodayAHoliday;
+	}
 
 }
